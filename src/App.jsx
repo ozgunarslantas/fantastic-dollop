0d30a37c44b40f6a109ecc15d2bc7abdb260e23b
ozgunarslantas <ozgun@picussecurity.com>
2020-04-16 01:44:00 +0300

    App is now jsx & use redux store

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,41 @@ import React from "react"
 import { Switch, Route, Link } from "react-router-dom"
 import wrongpage from "./wrongpage.jpg"
 
+import { connect } from "react-redux"
+import { boxMouseEnter } from "./actions"
+
 const LoginPage = () => <div>Login Page</div>
-const HomePage = () => <div>Home Page</div>
+const HomePage = connect(state => ({ values: state.values }), { boxMouseEnter })(
+  ({ values, boxMouseEnter }) => (
+    <div style={{ display: "flex", flex: 1, width: "100%", flexWrap: "wrap" }}>
+      {Array(16)
+        .fill(undefined)
+        .map((el, index) => {
+          const colors = { 0: "red", 1: "blue", 2: "yellow", 3: "green" }
+          const colorValue = (values[index] || 0) % 4
+          const bgColor = colors[colorValue]
+          return (
+            <div
+              key={index}
+              onMouseEnter={() => boxMouseEnter(index)}
+              style={{
+                width: "25%",
+                height: "25%",
+                justifyContent: "center",
+                alignItems: "center",
+                display: "flex",
+                cursor: "default",
+                backgroundColor: bgColor,
+                fontSize: 36,
+              }}
+            >
+              {values[index] || 0}
+            </div>
+          )
+        })}
+    </div>
+  ),
+)
 const NonHomePage = () => <div>NonHome Page</div>
 const WrongPage = () => (
   <div
