c7a7bafd20794340217f1a59b298d53fc3bb48a5
ozgunarslantas <ozgun@picussecurity.com>
2020-04-16 00:22:18 +0300

    Add navigation pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { BrowserRouter, Route, Link } from "react-router-dom"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const LoginPage = () => <div>Login Page</div>
+const HomePage = () => <div>Home Page</div>
+const NonHomePage = () => <div>NonHome Page</div>
+
+const App = () => (
+  <div
+    style={{
+      height: "100vh",
+      display: "flex",
+      flexDirection: "column",
+    }}
+  >
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-around",
+        alignItems: "center",
+        height: 64,
+        backgroundColor: "gray",
+      }}
+    >
+      <Link to="/login">Login</Link>
+      <Link to="/">Home</Link>
+      <Link to="/nonhome">NonHome</Link>
     </div>
-  );
-}
+    <div
+      style={{
+        backgroundColor: "red",
+        display: "flex",
+        flex: 1,
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Route path="/login" component={LoginPage} />
+      <Route path="/" exact component={HomePage} />
+      <Route path="/nonhome" component={NonHomePage} />
+    </div>
+  </div>
+)
 
-export default App;
+export default App
